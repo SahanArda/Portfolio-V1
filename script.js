f0078abca8a2f742dd3913d4ea6e1aa9f950b8c9
Arda Sahan <ardasahan@Ardas-MacBook-Pro.local>
2022-10-28 14:05:55 +0100

    the navbar is now fully responsive including mobile and tablet screens

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ resumeBtn.addEventListener("click", () => {
     window.open("https://www.google.com");
 });
 
+const menuHamburger = document.querySelector("#menu-hamburger");
+
+menuHamburger.addEventListener("click", () => {
+    const nav = document.querySelector("nav");
+    nav.classList.toggle("active");
+});
+
