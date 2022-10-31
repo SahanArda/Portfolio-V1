c731f0c815506187c8d444bc2274d5a1efed95bc
Arda Sahan <ardasahan@Ardas-MacBook-Pro.local>
2022-10-31 10:46:36 +0000

    removed unnecessary code and fixed svg blob

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const resumeBtn = document.querySelector("#resume-btn");
 
 resumeBtn.addEventListener("click", () => {
-    window.open("https://www.google.com");
+  window.open("https://www.google.com");
 });
 
 const menuHamburger = document.querySelector("#menu-hamburger");
 
 menuHamburger.addEventListener("click", () => {
-    const nav = document.querySelector("nav");
-    nav.classList.toggle("active");
+  const nav = document.querySelector("nav");
+  nav.classList.toggle("active");
 });
-
