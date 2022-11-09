ba487b8a6d5733b4a31b7df7523aa5c3679aa754
Arda Sahan <ardasahan@Ardas-MacBook-Pro.local>
2022-11-09 15:15:52 +0000

    made the navbar accessable while scrolling

--- script.js
+++ script.js
@@ -10,3 +10,24 @@ menuHamburger.addEventListener("click", () => {
   const nav = document.querySelector("nav");
   nav.classList.toggle("active");
 });
+
+const body = document.body;
+let lastScroll = 0;
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset
+  if (currentScroll <= 0) {
+    body.classList.remove("scroll-up")
+  }
+
+  if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+    body.classList.remove("scroll-up")
+    body.classList.add("scroll-down")
+  }
+
+  if (currentScroll < lastScroll && body.classList.contains("scroll-down")) {
+    body.classList.remove("scroll-down")
+    body.classList.add("scroll-up")
+  }
+
+  lastScroll = currentScroll;
+})
