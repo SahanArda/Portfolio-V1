ff419359a902788bfe52e0402f73194d164ed242
Arda Sahan <ardasahan@Ardas-MacBook-Pro.local>
2022-11-23 14:24:00 +0000

    resume button works

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-const resumeBtn = document.querySelector("#resume-btn");
-
-resumeBtn.addEventListener("click", () => {
-  window.open("https://www.google.com");
-});
-
 const menuHamburger = document.querySelector("#menu-hamburger");
 menuHamburger.addEventListener("click", () => {
   const nav = document.querySelector("nav");
